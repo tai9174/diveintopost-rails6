39e2e538db01cad717de27fb9d3d059cb558193f
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-08-01 21:25:28 +0900

    issue#1(仮2)

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :ensure_owner, only: %i[edit update]
 
   def index
     @teams = Team.all
@@ -55,5 +56,10 @@ class TeamsController < ApplicationController
 
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+  end
+
+  def ensure_owner
+    set_team
+    redirect_to teams_path, notice: 'チームリーダーのみ編集可能です' if @team.owner != current_user
   end
 end
