3a36a16dabe1585319aefe58d49b8e0e46f3c867
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-08-02 17:40:23 +0900

    agendas_controller.rbの実装

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -26,6 +26,13 @@ class AgendasController < ApplicationController
     if current_user.id == @agenda.user.id || current_user.id == @agenda.team.owner.id
       @agenda.destroy
       redirect_to dashboard_url, notice: "#{@agenda.title}を削除しました。"
+      @keep_team_id = @agenda.team_id
+      user_emails = User.where(keep_team_id: @keep_team_id).pluck(:email)
+      user_emails.each do |emails|
+        ContactMailer.contact_mail(emails).deliver
+      end
+    else
+      redirect_to dashboard_url, notice: "権限がないため削除できません。"
     end
   end
 
